b8a4eafc6526eed41b9a3f2e41ef508ee44faab4
pritam019 <pritamgayen629@gmail.com>
2023-09-01 16:14:26 +0530

    solution potd  sept 1

--- September_Solutions/Sep_1_potd_sol.cpp
+++ September_Solutions/Sep_1_potd_sol.cpp
@@ -1,7 +1,28 @@
+// QESTION : ->
+// Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
 #include<bits/stdc++.h>
 using namespace std;
-int main()
-{
 
-return 0;
-}
+// output 
+class Solution {
+public:
+    vector<int> countBits(int n) {
+       vector<int> ans(n + 1);
+    
+    for (int i = 0; i <= n; ++i) {
+        int count = 0;
+        int num = i;
+        
+        while (num > 0) {
+            if (num % 2 == 1) {
+                count++;
+            }
+            num /= 2;
+        }
+        
+        ans[i] = count;
+    }
+    
+    return ans;
+    }
+};
